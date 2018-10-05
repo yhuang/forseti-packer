97137fae628618800c2fd90b8b467aff266943ca
Jimmy Y. Huang <yhuang@duragility.attlocal.net>
2018-10-05 12:59:55 -0700

    patch code no longer needed

--- provisioning-scripts/install-forseti-security.sh
+++ provisioning-scripts/install-forseti-security.sh
@@ -12,9 +12,6 @@ FORSETI_INSTALL_DIR=$(pwd)
 git fetch --all
 git checkout tags/v$FORSETI_VERSION
 
-# Patch Forseti Security
-cp /tmp/cli.py $FORSETI_INSTALL_DIR/google/cloud/forseti/services/cli.py
-
 # Install Forseti Security
 python setup.py install
 
